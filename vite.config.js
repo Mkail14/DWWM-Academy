be9f8d68724124b2998a12c218d18b8d3ad448b5
Aloe VERA <aloe.vera@aloalo.alomayjob.com>
2026-06-22 14:15:35 +0300

    first

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'Mkail14/DWWM-Academy.git'
+  base: '/Mkail14/DWWM-Academy.git/'
 })
